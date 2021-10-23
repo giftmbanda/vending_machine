b96da503ce5f8d76bfc89d8f77431e3eaa85c69f
Gift Banda <giftmbanda@gmail.com>
2021-10-23 15:19:02 +0200

    Update app.ts

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,35 +1,35 @@
 import bodyParser from 'body-parser';
+import cors from "cors";
 import express, { Application, Request, Response } from 'express';
 import router from './routes/productRoute';
 import JSONResponse from './utils/JSONResponse';
-import cors from "cors";
 
 class App {
-    public express: Application;;
+    public app: Application;
 
     constructor() {
-        this.express = express();
-        this.express.use(cors());
-        this.express.use(bodyParser.json({ limit: '50mb' }));
+        this.app = express();
+        this.app.use(cors());
+        this.app.use(bodyParser.json({ limit: '50mb' }));
         this.loadRoutes();
     }
 
     private loadRoutes(): void {
 
         // handle routes
-        this.express.use('/', router);
+        this.app.use('/', router);
 
         // handle non-existence routes
-        this.express.use('*', (req: Request, res: Response) => {
+        this.app.use('*', (req: Request, res: Response) => {
             JSONResponse.notFound(req, res, 'Page not found')
         });
 
         // handle server error
-        this.express.use((req: Request, res: Response) => {
+        this.app.use((req: Request, res: Response) => {
             const error: Error = new Error();
             JSONResponse.serverError(req, res, error.message);
         });
     }
 }
 
-export default new App().express;
+export default new App().app;
